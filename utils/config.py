df6293cd8808e98a1ba36299111bd6cee5b5ad32
Mohtasim Hadi Rafi <mzr0167@auburn.edu>
2024-10-13 02:35:06 -0500

    env file introduced

--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,9 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+import os
+
 class Config:
-    MONGODB_URI = "mongodb://localhost:27017/"
-    CDN_URL = "http://localhost:8000/"
-    VIDEO_ANALYSIS_URL = "http://localhost:5000/"
+    MONGODB_URI         = os.getenv('MONGODB_URI')
+    CDN_URL             = os.getenv('CDN_URL')
+    VIDEO_ANALYSIS_URL  = os.getenv('VIDEO_ANALYSIS_URL')
